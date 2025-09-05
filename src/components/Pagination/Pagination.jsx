209ca2b80057472f0768cfd817b95b28cdb29d50
Alex Med <lexmedteh@gmail.com>
2025-09-05 11:17:57 +0300

    pagination smart

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -4,20 +4,51 @@ import { useAppContext } from "../../context/AppContext";
 import data from "../../data/data.json";
 
 export const Pagination = () => {
-  const { perPage, showNumPage, backwardPage, forwardPage,currentPage } = useAppContext();
+  const { perPage, showNumPage, backwardPage, forwardPage, currentPage, setCurrentPage } = useAppContext();
 
-  const pagesQuantity = Math.ceil(data.length / perPage);
+  // const pagesQuantity = Math.ceil(data.length / perPage);
+  const pagesQuantity = 10; //just for testing quantity pages
   const pagesQuantityList = [];
 
   for (let index = 1; index <= pagesQuantity; index++) {
     pagesQuantityList.push(index);
   }
-  
+
 
   return (
     <div className="pagination">
       <div className="pages">
-        {pagesQuantityList.map((page, idx) =>
+
+        {currentPage != 1 && currentPage != pagesQuantity ? (
+          <>
+            {currentPage > 2 && (
+              <>
+                <button className={currentPage === 1 && 'active'} onClick={() => setCurrentPage(1)}>1</button>
+                <span>...</span>
+              </>
+            )}
+            <button onClick={() => setCurrentPage(currentPage - 1)}>{currentPage - 1}</button>
+            <button className="active" >{currentPage}</button>
+            <button onClick={() => setCurrentPage(currentPage + 1)}>{currentPage + 1}</button>
+            {currentPage < pagesQuantity - 1 && (
+              <>
+                <span>...</span>
+                <button className={currentPage === pagesQuantity ? 'active': ''} onClick={() => setCurrentPage(pagesQuantity)}>{pagesQuantity}</button>
+              </>
+            )}
+
+          </>
+        ) : (
+          <>
+
+            <button className={currentPage === 1 && 'active'} onClick={() => setCurrentPage(1)}>1</button>
+            <button className={currentPage === pagesQuantity && 'active'} onClick={() => setCurrentPage(pagesQuantity)}>{pagesQuantity}</button>
+          </>
+        )}
+
+
+
+        {/* {pagesQuantityList.map((page, idx) =>
         
             <div className="page-current" key={idx}>
               {page===currentPage ?(
@@ -27,11 +58,7 @@ export const Pagination = () => {
               ): '...' }
              
             </div>
-
-
-
-         
-        )}
+        )} */}
       </div>
       <div className="btns">
         <button disabled={currentPage === 1} onClick={backwardPage}>
